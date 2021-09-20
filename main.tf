52c3563562b9eb66d7359f589eb40bd6f9ff0bae
jefferson-bit <jhonsonbabi@hotmail.com>
2021-09-20 14:49:15 -0300

    executando primeiro container

--- main.tf
+++ main.tf
@@ -11,6 +11,15 @@ provider "docker" {
   version = "~> 2.7"
   host    = "npipe:////.//pipe//docker_engine"
 }
+
 resource "docker_image" "docusaurus-zup" {
   name = "public.ecr.aws/zup-academy/docusaurus-zup:latest"
+}
+
+resource "docker_container" "docusaurus-zup" {
+  name  = "docusaurus-zup"
+  image = docker_image.docusaurus-zup.latest
+  ports{
+    internal = 3000
+  }
 }
