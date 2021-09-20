d3681c07d3b0747597a86752022dfaeb3e8c017f
jefferson-bit <jhonsonbabi@hotmail.com>
2021-09-20 14:27:10 -0300

    pull da imagem

--- main.tf
+++ main.tf
@@ -1,10 +1,16 @@
 terraform {
-    required_providers {
-      docker = {
-          source = "kreuzwerker/docker"
-          version = "2.14.0"
-      }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.14.0"
     }
+  }
 }
 
-provider "docker"{}
+provider "docker" {
+  version = "~> 2.7"
+  host    = "npipe:////.//pipe//docker_engine"
+}
+resource "docker_image" "docusaurus-zup" {
+  name = "public.ecr.aws/zup-academy/docusaurus-zup:latest"
+}
